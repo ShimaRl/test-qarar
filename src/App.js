dad02e2ea218164ac608bfb0b45cbda4cacbe0a7
Shima <shimavagheii@gmail.com>
2022-11-15 11:58:34 +0330

    jitsi added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { JitsiMeeting } from "@jitsi/react-sdk";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JitsiMeeting
+        roomName={"test"}
+        getIFrameRef={(node) => (node.style.height = "100vh")}
+        domain={"room.gharar.ir/e755ca18-30f1-4794-9da1-fa99ec84ab68"}
+      />
     </div>
   );
 }
